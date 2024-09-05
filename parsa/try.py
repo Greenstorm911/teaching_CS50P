98d0097b0f69173f0ddff4258b3c4d63e1a777fe
parsa <parsa.nazerrr@gmail.com>
2024-09-05 10:14:02 +0330

    try except

---
+++ parsa/try.py
@@ -0,0 +1,52 @@
+
+
+# try:
+#     print(username)
+#     user_number = int(input("enter the amount you want to charge your accout:"))
+# except ValueError:
+#     user_number = int(input("wrong input enter a cooerct number: "))
+
+# except NameError:
+#     username = input('you didnt enter username enter it:')
+#     user_number = int(input("enter the amount you want to charge your accout:"))
+
+
+# def get_int():
+#     #loop and try except get number
+#     while True:
+#         try:
+#             number = int(input())
+#         except ValueError:
+#             print("wrong input enter a number")
+#     return number
+
+# def main():
+#     number = get_int()
+#     print(number)
+
+# main()
+
+
+# def get_int():
+#     while True:
+#         try:
+#             number = int(input())
+#             if number > 0:
+#                 break
+#         except ValueError:
+#             print('wrong input')
+#     return number
+
+
+# print(get_int())
+
+
+user_string = "parsa nazer"
+
+for char in user_string:
+    print(char, end='')
+
+
+def print(string, end='\n'):
+    
+
